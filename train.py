67150fb0a7990a4e497d2a95c59d29a19177bd3d
SM <smsm8898@ajou.ac.kr>
2022-03-20 18:36:50 +0900

    pytorch

--- train.py
+++ train.py
@@ -6,91 +6,59 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-
-# Create fully connected network
-class NN(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(NN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 50)
-        self.fc2 = nn.Linear(50, num_classes)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-# model = NN(784, 10)
-# x = torch.randn(64, 784)
-# print(model(x).shape)
-
-
-def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
-    print("=> Saving checkpoint")
-    torch.save(state, filename)
-
-def load_checkpoint(checkpoint):
-    model.load_state_dict(checkpoint['state_dict'])
-    optimizer.load_state_dict(checkpoint['state_dict'])
+import torchvision
+from customDataset import CatsAndDogsDataset
 
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameter
-input_size = 784
+in_channel = 3
 num_classes = 10
-learning_rate = 0.001
+learning_rate = 1e-3
 batch_size = 64
 num_epochs = 1
-load_model = True
 
 # Load Data
-train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+dataset = CatsAndDogsDataset(csv_file='dataset/cats_dogs.csv', root_dir='dataset/cats_dogs_resized', transform=transforms.ToTensor())
+train_set, test_set = torch.utils.data.random_split(dataset, [8, 2])
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
 # Initialize network
-model = NN(input_size, num_classes).to(device)
+model = torchvision.models.googlenet(pretrained=True)
+model.to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-if load_model:
-    load_model(torch.load("my_checkpoint.pth.tar"))
 
 # Train network
 for epoch in range(num_epochs):
     losses = []
-
-    if epoch % 3:
-        checkpoint = {'state_dict': model.state_dict(),
-                      'optimizer': optimizer.state_dict()}
-        save_checkpoint(checkpoint)
-
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device)
         targets = targets.to(device)
 
-        data = data.reshape(data.shape[0], -1)
         # forward
         scores = model(data)
         loss = criterion(scores, targets)
         losses.append(loss)
-
+        
         # backward
         optimizer.zero_grad()
         loss.backward()
 
         # gradient descent or adam step
         optimizer.step()
-        # print(loss.item())
+    print(f'Cost at epoch {epoch+1} is {sum(losses)/len(losses):.4f}')
 
 # Check accuracy on training & test to see how good our model
 def check_accuracy(loader, model):
-    if loader.dataset.train:
-        print("Checking accuracy on train data")
-    else:
-        print("Checking accuracy on test data")
+    # if loader.dataset.train:
+    #     print("Checking accuracy on train data")
+    # else:
+    #     print("Checking accuracy on test data")
     num_correct = 0
     num_samples = 0
     model.eval()
@@ -99,7 +67,6 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device)
             y = y.to(device)
-            x = x.reshape(x.shape[0], -1)
 
             scores = model(x) # [64, 10]
             _, predictions = scores.max(1)
